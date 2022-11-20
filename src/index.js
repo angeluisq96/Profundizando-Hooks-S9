5493c358b9c584899ccd6b1087e224e6f5bcf067
Angel Luis <angel.luis@mercadolibre.com>
2022-11-20 08:36:08 -0300

    main

--- src/index.js
+++ src/index.js
@@ -1,29 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-
-// import { HooksApp } from './HooksApp';
-// import { CounterApp } from './01-useState/CounterApp';
-// import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
-// import { SimpleForm } from './02-useEffect/SimpleForm';
-// import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
-// import { MultipleCustomHooks } from './03-examples/index';
-// import { FocusScreen } from './04-useRef/FocusScreen';
-// import { Layout } from './05-useLayoutEfect/Layout';
-// import { Memorize } from './06-memos/Memorize';
-// import { MemorizeHook } from './06-memos/MemorizeHook';
-// import { CallbackHook } from './06-memos/CallbackHook';
-// import { Padre } from './07-tarea-memo/Padre';
-// import './08-useReducer/intro-reducer'
-// import { TodoApp } from './08-useReducer/TodoApp';
-import { MainApp } from './09-useContext/MainApp';
-
-
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
-        <MainApp />
-    </BrowserRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
