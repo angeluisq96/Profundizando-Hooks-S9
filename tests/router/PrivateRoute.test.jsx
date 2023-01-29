f11a1b8ac9525696fc670e0e96635bee577473dd
Angel Luis <angel.luis@mercadolibre.com>
2023-01-29 13:36:46 -0300

    Test en PrivateRoute

---
+++ tests/router/PrivateRoute.test.jsx
@@ -0,0 +1,31 @@
+const { render, screen } = require('@testing-library/react')
+import { MemoryRouter } from 'react-router-dom';
+import { AuthContext } from '../../src/auth';
+import { PrivateRoute } from '../../src/router/PrivateRoute';
+
+describe('Pruebas en el PrivateRoute', () => {
+    test('Debe mostrar el children si esta autenticado', () => {
+
+        Storage.prototype.setItem = jest.fn();
+        const contextValue = {
+            logged: true,
+            user: {
+                name: 'superman',
+                id: 1,
+            }
+        }
+
+        render(
+            <AuthContext.Provider value={contextValue}>
+                <MemoryRouter initialEntries={['/marvel']}>
+                    <PrivateRoute>
+                        <h1>Private Route</h1>
+                    </PrivateRoute>
+                </MemoryRouter>
+            </AuthContext.Provider>
+        );
+
+        expect(screen.getByText('Private Route')).toBeTruthy()
+        expect( localStorage.setItem ).toHaveBeenCalledWith('lastPath', '/marvel')
+    })
+})
