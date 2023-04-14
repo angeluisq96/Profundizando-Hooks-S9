1e2ebea16e8581e4c8a4522c561fb804033fca1d
unknown <angeluisq96@gmail.com>
2023-04-14 18:41:22 -0300

    startGoogleSingIn when its true and false

--- tests/store/auth/thunks.test.js
+++ tests/store/auth/thunks.test.js
@@ -1,5 +1,7 @@
-import { checkingCredentials } from "../../../src/store/auth/authSlice";
-import { checkingAuthentication } from "../../../src/store/auth/thunks";
+import { singInWithGoogle } from "../../../src/firebase/providers";
+import { checkingCredentials, login, logout } from "../../../src/store/auth/authSlice";
+import { checkingAuthentication, startGoogleSingIn } from "../../../src/store/auth/thunks";
+import { userTests } from '../../fixtures/authFixtures'
 
 jest.mock('../../../src/firebase/providers')
 
@@ -13,7 +15,25 @@ describe('Tests auth thunks', () => {
     expect( dispatch ).toHaveBeenCalledWith( checkingCredentials() ) ;
   } ) ;
 
+  test('should first startGoogleSingin call checkingCredentials and login Done', async() => {
+    const loginData = { ok:true, ...userTests } ;
+    await singInWithGoogle.mockResolvedValue( loginData ) ;
+    await startGoogleSingIn()( dispatch )
 
+    expect( dispatch ).toHaveBeenCalledWith( checkingCredentials() ) ;
+    expect( dispatch ).toHaveBeenCalledWith( login( loginData ) ) ;
+  } ) ;
 
+  test('should first startGoogleSingin call checkingCredentials and login Error', async() => {
+    const loginData = { ok:false, errorMessage: 'An error in google' } ;
+    await singInWithGoogle.mockResolvedValue( loginData ) ;
+    await startGoogleSingIn()( dispatch )
+
+    const queEs = logout( loginData.errorMessage )
+    console.log(queEs)
+
+    // expect( dispatch ).toHaveBeenCalledWith( checkingCredentials() ) ;
+    expect( dispatch ).toHaveBeenCalledWith( logout( loginData.errorMessage ) ) ;
+  } ) ;
 
 } ) ;
